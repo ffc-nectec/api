c593162029fb2e9150d9a457fd7a983361ffdeae
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-07-09 14:59:17 +0700

    Fix check block user. "ADM", "adm", "newuser", "usr_db", "Drug_Store_Admin"

--- src/main/java/ffc.airsync.api/services/module/UserService.kt
+++ src/main/java/ffc.airsync.api/services/module/UserService.kt
@@ -1,9 +1,11 @@
 package ffc.airsync.api.services.module
 
+import ffc.airsync.api.dao.UserDao
 import ffc.airsync.api.printDebug
 import ffc.entity.Token
 import ffc.entity.User
 import java.util.ArrayList
+import javax.ws.rs.ForbiddenException
 import javax.ws.rs.NotAuthorizedException
 
 object UserService {
@@ -18,7 +20,7 @@ object UserService {
     }
 
     fun login(orgId: String, username: String, pass: String): Token {
-
+        if (UserDao.isBlockUser(username)) throw ForbiddenException("User ไม่มีสิทธิ์ในการใช้งาน")
         val user = orgUser.getUser(username, pass, orgId)
         if (user != null) {
             return tokenDao.create(user, orgId)
