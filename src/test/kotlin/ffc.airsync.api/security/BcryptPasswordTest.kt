6be075f822ee3ff15f3e5c775ea057465925646a
Piruin Panichphol <piruin.p@gmail.com>
2018-07-11 16:36:52 +0700

    format

--- src/test/kotlin/ffc.airsync.api/security/BcryptPasswordTest.kt
+++ src/test/kotlin/ffc.airsync.api/security/BcryptPasswordTest.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ffc.airsync.api.security
 
 import org.amshove.kluent.`should be`
@@ -16,8 +33,8 @@ class BcryptPasswordTest(val plain: String) {
         @Parameterized.Parameters(name = "{0}")
         fun data() = listOf(
                 arrayOf("123456"),
-                arrayOf("qwerty") ,
-                arrayOf("password") ,
+                arrayOf("qwerty"),
+                arrayOf("password"),
                 arrayOf("h()!yshiTP@ssw0rd")
         )
     }
@@ -34,5 +51,3 @@ class BcryptPasswordTest(val plain: String) {
         password.hash(plain) `should not be equal to` password.hash(plain)
     }
 }
-
-
