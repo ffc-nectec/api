20e5e1eeb701675d5a314b12fa0f31cf8ca8c6bd
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-09-19 10:57:21 +0700

    ปรับ foreach เป็น map ใน HealthCareServiceDao

--- src/main/kotlin/ffc.airsync.api/dao/HealthCareServiceDao.kt
+++ src/main/kotlin/ffc.airsync.api/dao/HealthCareServiceDao.kt
@@ -5,9 +5,9 @@ import ffc.entity.healthcare.HealthCareService
 interface HealthCareServiceDao : Dao {
     fun insert(healthCareService: HealthCareService, orgId: String): HealthCareService
     fun insert(healthCareService: List<HealthCareService>, orgId: String): List<HealthCareService> {
-        val result = arrayListOf<HealthCareService>()
-        healthCareService.forEach { result.add(insert(it, orgId)) }
-        return result
+        return healthCareService.map {
+            insert(it, orgId)
+        }
     }
 
     fun find(id: String, orgId: String): HealthCareService?
