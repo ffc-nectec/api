117d2593ae7a2025ac43c9a3df3b72652105cd15
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-09-26 15:10:15 +0700

    fix Error Filter

--- src/main/kotlin/ffc.airsync.api/services/filter/ErrorFilter.kt
+++ src/main/kotlin/ffc.airsync.api/services/filter/ErrorFilter.kt
@@ -3,6 +3,7 @@ package ffc.airsync.api.services.filter
 import ffc.airsync.api.printDebug
 import javax.ws.rs.BadRequestException
 import javax.ws.rs.ForbiddenException
+import javax.ws.rs.NotAllowedException
 import javax.ws.rs.NotAuthorizedException
 import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.MediaType
@@ -52,7 +53,7 @@ class RequireError : ExceptionMapper<IllegalArgumentException> {
     override fun toResponse(exception: IllegalArgumentException?): Response {
         exception!!.printStackTrace()
         val err = ErrorFilter.ErrorRes(400, exception.message, exception)
-        val except = BadRequestException(err.message)
+        val except = NotAllowedException(err.message)
         return Response.status(except.response.statusInfo).entity(err).type(MediaType.APPLICATION_JSON_TYPE).build()
     }
 }
