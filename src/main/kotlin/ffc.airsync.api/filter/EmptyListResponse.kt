583b6743a41e787df2dc36498f8f7c2678d65e14
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-10-03 14:18:18 +0700

    rename Empty list response

---
+++ src/main/kotlin/ffc.airsync.api/filter/EmptyListResponse.kt
@@ -0,0 +1,22 @@
+package ffc.airsync.api.filter
+
+import javax.annotation.Priority
+import javax.ws.rs.NotFoundException
+import javax.ws.rs.Priorities
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.container.ContainerResponseContext
+import javax.ws.rs.container.ContainerResponseFilter
+import javax.ws.rs.ext.Provider
+
+@Suppress("UNUSED_VARIABLE")
+@Priority(Priorities.ENTITY_CODER)
+@Provider
+class EmptyListResponse : ContainerResponseFilter {
+    override fun filter(
+        requestContext: ContainerRequestContext,
+        responseContext: ContainerResponseContext
+    ) {
+        val list = responseContext.entity as? List<*> ?: return
+        if (list.isEmpty()) throw NotFoundException()
+    }
+}
