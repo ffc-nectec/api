fa6471eb188c680822b39c88af7ea83271351676
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-11-08 13:21:05 +0700

    Dev remove sort row genogram.

--- src/main/kotlin/ffc.airsync.api/services/personrelationsship/GenogramUtil.kt
+++ src/main/kotlin/ffc.airsync.api/services/personrelationsship/GenogramUtil.kt
@@ -6,51 +6,23 @@ import ffc.entity.Person.Relate.Divorced
 import ffc.entity.Person.Relate.Married
 import kotlin.math.absoluteValue
 
-internal fun Map<Int, ArrayList<Person>>.toList(): List<Person> {
-    val list = arrayListOf<Person>()
-    forEach { key, value ->
-        value.forEach {
-            list.add(it)
-        }
-    }
-    return list
-}
-
 internal fun List<Person>.deep(): Map<Int, ArrayList<Person>> {
     val result = hashMapOf<String, GenogramProcessProperty>()
     calDeep(first(), this, result)
     return groupDeep(result)
 }
 
-private typealias GroupXarrayList = HashMap<Int, ArrayList<Person>>
-
 private fun groupDeep(result: HashMap<String, GenogramProcessProperty>): Map<Int, ArrayList<Person>> {
     val groupDeep = hashMapOf<Int, ArrayList<Person>>()
 
-    val groupSuperDeep = hashMapOf<Int, GroupXarrayList>()
-
+    var min = 0
     result.forEach { key, value ->
         val deep = value.deep
-        val x = value.x
-        if (groupSuperDeep[deep] == null)
-            groupSuperDeep[deep] = hashMapOf()
-
-        if (groupSuperDeep[deep]!![x] == null)
-            groupSuperDeep[deep]!![x] = arrayListOf()
-
-        groupSuperDeep[deep]!![x]!!.add(value.person)
-    }
-
-    var min = 0
-
-    groupSuperDeep.forEach { deep, value ->
         if (deep < min) min = deep
         if (groupDeep[deep] == null)
             groupDeep[deep] = arrayListOf()
 
-        value.toSortedMap().forEach { key, listPerson ->
-            listPerson.forEach { groupDeep[deep]!!.add(it) }
-        }
+        groupDeep[deep]!!.add(value.person)
     }
     min = min.absoluteValue + 1
 
@@ -61,38 +33,37 @@ private fun calDeep(
     person: Person?,
     listPerson: List<Person>,
     result: HashMap<String, GenogramProcessProperty>,
-    deep: Int = 0,
-    x: Int = 0
+    deep: Int = 0
 ) {
     if (person == null) return
     if (person.bundle["genogram"] == "lock") return
     person.bundle["genogram"] = "lock"
 
-    result[person.id] = GenogramProcessProperty(person, deep, x)
+    result[person.id] = GenogramProcessProperty(person, deep)
 
     person.fatherId?.let { id ->
-        calDeep(listPerson.find { it.id == id }, listPerson, result, deep - 1, x - 1)
+        calDeep(listPerson.find { it.id == id }, listPerson, result, deep - 1)
     }
 
     person.motherId?.let { id ->
-        calDeep(listPerson.find { it.id == id }, listPerson, result, deep - 1, x)
+        calDeep(listPerson.find { it.id == id }, listPerson, result, deep - 1)
     }
 
     person.relationships.filter { it.relate == Married }.forEach { rela ->
-        calDeep(listPerson.find { it.id == rela.id }, listPerson, result, deep, x - 1)
+        calDeep(listPerson.find { it.id == rela.id }, listPerson, result, deep)
     }
 
     person.relationships.filter { it.relate == Divorced }.forEach { rela ->
-        calDeep(listPerson.find { it.id == rela.id }, listPerson, result, deep, x + 1)
+        calDeep(listPerson.find { it.id == rela.id }, listPerson, result, deep)
     }
 
     person.relationships.filter { it.relate == Child }.forEach { rela ->
-        calDeep(listPerson.find { it.id == rela.id }, listPerson, result, deep + 1, x + 1)
+        calDeep(listPerson.find { it.id == rela.id }, listPerson, result, deep + 1)
     }
 
     person.bundle.remove("genogram")
 }
 
-private data class GenogramProcessProperty(val person: Person, var deep: Int, var x: Int) {
+private data class GenogramProcessProperty(val person: Person, var deep: Int) {
     val name = person.name
 }
