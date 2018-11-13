cd7c00996b05ea3af97dfa7681cb43b86627eb25
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-11-13 10:54:13 +0700

    fix @Developer

--- src/main/kotlin/ffc.airsync.api/filter/Developer.kt
+++ src/main/kotlin/ffc.airsync.api/filter/Developer.kt
@@ -1,3 +1,17 @@
 package ffc.airsync.api.filter
 
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.ws.rs.NameBinding
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FILE
+)
+
 annotation class Developer
