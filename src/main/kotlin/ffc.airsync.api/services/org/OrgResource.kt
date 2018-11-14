d1852172da0cc70c62363fed75b801295e2a45df
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-11-14 16:39:46 +0700

    fix OrgResource default value

--- src/main/kotlin/ffc.airsync.api/services/org/OrgResource.kt
+++ src/main/kotlin/ffc.airsync.api/services/org/OrgResource.kt
@@ -25,6 +25,7 @@ import javax.annotation.security.RolesAllowed
 import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.Consumes
 import javax.ws.rs.DELETE
+import javax.ws.rs.DefaultValue
 import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
@@ -57,7 +58,7 @@ class OrgResource {
 
     @GET
     @Cache(maxAge = 5)
-    fun get(@QueryParam("my") my: Boolean = false, @QueryParam("query") query: String?): List<Organization> {
+    fun get(@QueryParam("my") @DefaultValue("false") my: Boolean, @QueryParam("query") query: String?): List<Organization> {
         return if (my) {
             printDebug("Find Organization with ip-address = ${req.ipAddress}")
             OrgService.getMy(req.ipAddress)
