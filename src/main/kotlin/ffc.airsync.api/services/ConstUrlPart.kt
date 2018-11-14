0911e89d22da928abc5e1d9371e2be3aae7e2bae
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-11-14 16:10:08 +0700

    fix Village part

--- src/main/kotlin/ffc.airsync.api/services/ConstUrlPart.kt
+++ src/main/kotlin/ffc.airsync.api/services/ConstUrlPart.kt
@@ -3,3 +3,4 @@ package ffc.airsync.api.services
 const val ORGIDTYPE = "{orgId:([\\dabcdefABCDEF]+)}"
 const val PERSONIDTYPE = "{personId:([\\dabcdefABCDEF]+)}"
 const val VISITIDTYPE = "{visitId:([\\dabcdefABCDEF]+)}"
+const val VILLAGETYPE = "{villageId:([\\dabcdefABCDEF]+)"
