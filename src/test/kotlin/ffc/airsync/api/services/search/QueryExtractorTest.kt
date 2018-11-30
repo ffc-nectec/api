f0483a118449cce8a0c172c0dbe06f90b9f5e0c4
Piruin Panichphol <piruin.p@gmail.com>
2018-11-30 13:12:27 +0700

    Can extract query parameter from string Elder(age), DM and HT

---
+++ src/test/kotlin/ffc/airsync/api/services/search/QueryExtractorTest.kt
@@ -0,0 +1,24 @@
+package ffc.airsync.api.services.search
+
+import org.amshove.kluent.`should have value`
+import org.junit.Test
+
+class QueryExtractorTest {
+
+    val extractor = QueryExtractor()
+
+    @Test
+    fun extract() {
+        val query = extractor.extract("ผู้สูงอายุที่เป็นโรคเบาหวาน")
+
+        query `should have value` Query("age", 60, Query.Operator.MORE_THAN)
+        query `should have value` Query("dm", true)
+    }
+
+    @Test
+    fun extract2() {
+        val query = extractor.extract("ผู้ป่วยความดันโลหิตสูง")
+
+        query `should have value` Query("ht", true)
+    }
+}
