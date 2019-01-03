fa84df495d25d23fe50375c8bd7ca9ec144f131f
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-01-03 15:31:50 +0700

    add create template individual hos

--- src/main/kotlin/ffc.airsync.api/services/template/TemplateResource.kt
+++ src/main/kotlin/ffc.airsync.api/services/template/TemplateResource.kt
@@ -3,9 +3,12 @@ package ffc.airsync.api.services.template
 import ffc.airsync.api.filter.Cache
 import ffc.airsync.api.services.ORGIDTYPE
 import ffc.entity.Template
+import javax.annotation.security.RolesAllowed
 import javax.ws.rs.Consumes
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
@@ -18,7 +21,21 @@ class TemplateResource {
     @GET
     @Path("/$ORGIDTYPE/template")
     @Cache(maxAge = 3600)
-    fun find(@QueryParam("query") query: String?): List<Template> {
-        return templates.find(query ?: "")
+    fun find(
+        @PathParam("orgId") orgId: String,
+        @QueryParam("query") query: String?
+    ): List<Template> {
+        return templates.find(orgId, query ?: "")
+    }
+
+    @POST
+    @Path("/$ORGIDTYPE/template")
+    @RolesAllowed("ORG", "ADMIN")
+    fun clearAndInsert(
+        @PathParam("orgId") orgId: String,
+        template: List<Template>
+    ) {
+        templates.removeByOrgId(orgId)
+        templates.insert(orgId, template)
     }
 }
