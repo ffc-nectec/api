0ea373eec38a8651a5d2147d2cb534b39ca64837
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-02-14 15:34:27 +0700

    fix bug Ip address.

--- src/main/kotlin/ffc.airsync.api/services/util/IpAddress.kt
+++ src/main/kotlin/ffc.airsync.api/services/util/IpAddress.kt
@@ -3,5 +3,5 @@ package ffc.airsync.api.services.util
 import javax.servlet.http.HttpServletRequest
 
 fun HttpServletRequest.ipAddress(): String {
-    return getHeader("X-Forwarded-For") ?: remoteAddr
+    return getHeader("X-Real-IP") ?: (getHeader("X-Forwarded-For") ?: remoteAddr)
 }
