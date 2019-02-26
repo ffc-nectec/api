cef33f4f3a0236b279af91ece0c9ce8c33641a7b
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-02-26 09:59:10 +0700

    upload image by async

--- src/main/kotlin/ffc.airsync.api/services/image/ImageResource.kt
+++ src/main/kotlin/ffc.airsync.api/services/image/ImageResource.kt
@@ -1,13 +1,17 @@
 package ffc.airsync.api.services.image
 
 import ffc.airsync.api.FFCApiServer
+import ffc.airsync.api.filter.ErrorDetail
 import ffc.airsync.api.getLogger
 import ffc.airsync.api.services.ORGIDTYPE
 import ffc.airsync.api.services.util.getUserLogin
 import ffc.entity.Template
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition
 import org.glassfish.jersey.media.multipart.FormDataParam
+import org.glassfish.jersey.server.ManagedAsync
 import java.io.InputStream
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 import javax.annotation.security.RolesAllowed
 import javax.ws.rs.Consumes
 import javax.ws.rs.DefaultValue
@@ -15,6 +19,8 @@ import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.container.AsyncResponse
+import javax.ws.rs.container.Suspended
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
@@ -29,6 +35,7 @@ class ImageResource {
     val logger = getLogger()
 
     @POST
+    @ManagedAsync
     @Path("/$ORGIDTYPE/image")
     @RolesAllowed("USER", "ORG", "ADMIN", "PROVIDER", "SURVEYOR", "PATIENT")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -36,12 +43,24 @@ class ImageResource {
         @DefaultValue("true") @FormDataParam("enabled") enabled: Boolean,
         @FormDataParam("file") file: InputStream,
         @FormDataParam("file") fileDetail: FormDataContentDisposition,
-        @PathParam("orgId") orgId: String
-    ): Response {
+        @PathParam("orgId") orgId: String,
+        @Suspended asyncResponse: AsyncResponse
+    ) {
+        asyncResponse.setTimeout(15, TimeUnit.SECONDS)
+        asyncResponse.setTimeoutHandler {
+            val ex = TimeoutException("Time Out")
+            val timeOut = ErrorDetail(Response.Status.SERVICE_UNAVAILABLE.statusCode, ex.message, ex)
+            it.resume(Response.status(timeOut.code).entity(timeOut).build())
+        }
+        val result = postImage(file, orgId)
+        asyncResponse.resume(result)
+    }
+
+    private fun postImage(file: InputStream, orgId: String): Response {
         val reqBody = createRequestStream(file)
-        val response = okRun("""${FFCApiServer.thumborUrl}/image""", reqBody)
-        val fileLocation = Template("Location", response.header("Location") ?: "")
+        val rawResponse = okRun("""${FFCApiServer.thumborUrl}/image""", reqBody)
+        val fileLocation = Template("Location", rawResponse.header("Location") ?: "")
         logger.info("Create image by User:${context.getUserLogin()} Org:$orgId Method:postimage")
-        return Response.status(200).entity(fileLocation).build()
+        return Response.status(201).entity(fileLocation).build()
     }
 }
