9a84c5ac026a50ecdc644aebf92a80f2da7ed842
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-02 10:48:59 +0700

    fix bug login ป้องกันไม่ให้ token ต่าง org เข้าถึง org ที่ไม่ใช่ของตัวเอง

--- src/main/kotlin/ffc.airsync.api/Log.kt
+++ src/main/kotlin/ffc.airsync.api/Log.kt
@@ -3,6 +3,10 @@ package ffc.airsync.api
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
+inline fun <reified T> getLoggerC(clazz: T): Logger {
+    return LogManager.getLogger(T::class)
+}
+
 fun Any.getLogger(): Logger {
     return LogManager.getLogger(this::class.java)
 }
