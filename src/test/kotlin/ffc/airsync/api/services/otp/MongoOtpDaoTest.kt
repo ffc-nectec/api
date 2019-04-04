29ee758a1e3132c2e697d410e571b1caf87217e1
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-04 12:37:23 +0700

    add next otp

--- src/test/kotlin/ffc/airsync/api/services/otp/MongoOtpDaoTest.kt
+++ src/test/kotlin/ffc/airsync/api/services/otp/MongoOtpDaoTest.kt
@@ -1,8 +1,10 @@
 package ffc.airsync.api.services.otp
 
 import ffc.airsync.api.MongoDbTestRule
+import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should not be equal to`
+import org.amshove.kluent.`should not be in`
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -55,5 +57,17 @@ class MongoOtpDaoTest {
         val otp = dao.get(ORG_ID)
         Thread.sleep(150)
         dao.validateOk(ORG_ID, otp) `should be equal to` false
+    }
+
+    @Test
+    fun forceNextOtp() {
+        val check = arrayListOf<String>()
+        runBlocking {
+            repeat(100) {
+                val otp = dao.forceNextOpt(ORG_ID)
+                otp `should not be in` check
+                check.add(otp)
+            }
+        }
     }
 }
