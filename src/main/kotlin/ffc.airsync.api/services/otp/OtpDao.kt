dc36968f434ef637e764e17c3c5906a246944abf
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-04 12:37:23 +0700

    add next otp

--- src/main/kotlin/ffc.airsync.api/services/otp/OtpDao.kt
+++ src/main/kotlin/ffc.airsync.api/services/otp/OtpDao.kt
@@ -2,5 +2,6 @@ package ffc.airsync.api.services.otp
 
 interface OtpDao {
     fun get(orgId: String): String
+    fun forceNextOpt(orgId: String): String
     fun validateOk(orgId: String, otp: String): Boolean
 }
