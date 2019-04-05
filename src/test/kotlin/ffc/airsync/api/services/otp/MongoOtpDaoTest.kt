1c72ed25fb5ca70ed0a513f27836628a1f8f0a65
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-05 14:25:56 +0700

    update otp

--- src/test/kotlin/ffc/airsync/api/services/otp/MongoOtpDaoTest.kt
+++ src/test/kotlin/ffc/airsync/api/services/otp/MongoOtpDaoTest.kt
@@ -1,7 +1,10 @@
 package ffc.airsync.api.services.otp
 
 import ffc.airsync.api.MongoDbTestRule
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should equal`
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -13,11 +16,12 @@ class MongoOtpDaoTest {
     val mongo = MongoDbTestRule()
 
     private val ORG_ID = "5bbd7f5ebc920637b04c7796"
+    private val ORG_ID2 = "5bbd7f5ebc920637b04c7799"
     lateinit var dao: OtpDao
 
     @Before
     fun initDb() {
-        dao = MongoOtpDao(100, TimeUnit.MILLISECONDS)
+        dao = MongoOtpDao(10, TimeUnit.SECONDS)
     }
 
     @Test
@@ -41,18 +45,50 @@ class MongoOtpDaoTest {
     @Test
     fun getNotExpire() {
         val otp = dao.get(ORG_ID)
+        Thread.sleep(5000)
         dao.isValid(ORG_ID, otp) `should be equal to` true
     }
 
     @Test
     fun getExpire() {
         val otp = dao.get(ORG_ID)
-        Thread.sleep(150)
+        Thread.sleep(15000)
         dao.isValid(ORG_ID, otp) `should be equal to` false
     }
 
     @Test
     fun validateFail() {
         dao.isValid(ORG_ID, "333333") `should be equal to` false
+    }
+
+    @Test
+    fun doNotAcceptOtherOrganizations() {
+        val otp1 = dao.get(ORG_ID)
+        val otp2 = dao.get(ORG_ID2)
+
+        dao.isValid(ORG_ID, otp2) `should be equal to` false
+        dao.isValid(ORG_ID2, otp1) `should be equal to` false
+    }
+
+    @Test
+    fun acceptMyOrganizations() {
+        val otp1 = dao.get(ORG_ID)
+        val otp2 = dao.get(ORG_ID2)
+
+        dao.isValid(ORG_ID, otp1) `should be equal to` true
+        dao.isValid(ORG_ID2, otp2) `should be equal to` true
+    }
+
+    @Test
+    fun get2OtpButAcceptOtp1_Otp1NotExpire() {
+        val otpFirst = dao.get(ORG_ID)
+        dao.get(ORG_ID2)
+        runBlocking {
+            delay(5000)
+        }
+        val otpSecount = dao.get(ORG_ID)
+        println("otp1 $otpFirst otp2 $otpSecount")
+        dao.isValid(ORG_ID, otpFirst) `should be equal to` true
+        otpFirst `should equal` otpSecount
     }
 }
