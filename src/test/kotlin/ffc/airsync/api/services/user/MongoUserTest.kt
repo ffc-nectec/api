4f780b6d750dc632cf7801a1f63e38090f0d13c5
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-24 14:56:04 +0700

    เพิ่มการตรวจสอบ activate ใน userDao และ OrgDao

--- src/test/kotlin/ffc/airsync/api/services/user/MongoUserTest.kt
+++ src/test/kotlin/ffc/airsync/api/services/user/MongoUserTest.kt
@@ -1,12 +1,32 @@
+/*
+ * Copyright (c) 2562 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package ffc.airsync.api.services.user
 
 import ffc.airsync.api.MongoDbTestRule
 import ffc.airsync.api.services.org.MongoOrgDao
+import ffc.airsync.api.services.org.OrgDao
 import ffc.entity.Organization
 import ffc.entity.User
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should equal`
 import org.amshove.kluent.`should not equal`
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,40 +39,46 @@ class MongoUserTest {
 
     private lateinit var nectecOrg: Organization
     private lateinit var dao: MongoUserDao
+    private lateinit var orgDao: OrgDao
 
     @Before
     fun initDb() {
+        println("Setup db")
         dao = MongoUserDao()
         val org = Org("รพสตNectec", "192.168.99.3")
-        nectecOrg = MongoOrgDao().insert(org)
+        orgDao = MongoOrgDao()
+        nectecOrg = orgDao.insert(org)
     }
 
-    companion object {
-        fun Org(name: String = "NECTEC", ip: String = "127.0.01"): Organization =
-            Organization().apply {
-                this.name = name
-                bundle["lastKnownIp"] = ip // "203.111.222.123"
-                this.users = userList.toMutableList()
-            }
-
-        val userList = listOf(
-            User("maxkung", User.Role.ADMIN),
-            User("somYing"),
-            User("somChai"),
-            User("adm"),
-            User("ADM"),
-            User("newuser"),
-            User("usr_db"),
-            User("Drug_Store_Admin")
-        )
-
-        fun User(name: String, role: User.Role = User.Role.PROVIDER): User =
-            User().apply {
-                this.name = name
-                password = "catbite"
-                this.roles.add(role)
-            }
+    @After
+    fun tearDown() {
+        orgDao.remove(nectecOrg.id)
     }
+
+    fun Org(name: String = "NECTEC", ip: String = "127.0.01"): Organization =
+        Organization().apply {
+            this.name = name
+            bundle["lastKnownIp"] = ip // "203.111.222.123"
+            this.users = userList.toMutableList()
+        }
+
+    val userList = listOf(
+        createUser("maxkung", User.Role.ADMIN),
+        createUser("somYing"),
+        createUser("somChai"),
+        createUser("adm"),
+        createUser("ADM"),
+        createUser("newuser"),
+        createUser("usr_db"),
+        createUser("Drug_Store_Admin")
+    )
+
+    fun createUser(name: String, role: User.Role = User.Role.PROVIDER): User =
+        User().apply {
+            this.name = name
+            password = "catbite"
+            this.roles.add(role)
+        }
 
     @Test
     fun findAll() {
@@ -63,22 +89,30 @@ class MongoUserTest {
 
     @Test
     fun insertUser() {
-        val user = dao.insertUser(User("Sommai"), nectecOrg.id)
+        val user = dao.insertUser(createUser("Sommai"), nectecOrg.id)
 
         user.name `should be equal to` "Sommai"
         user.password `should not equal` null
+        user.isActivated `should be equal to` false
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun insertUserActivateCheckFail() {
+        dao.insertUser(createUser("Sommai").apply {
+            activate()
+        }, nectecOrg.id)
     }
 
     @Test
     fun getUserById() {
-        val user = dao.insertUser(User("Sommai"), nectecOrg.id)
+        val user = dao.insertUser(createUser("Sommai"), nectecOrg.id)
 
         dao.getUserById(nectecOrg.id, user.id).name `should be equal to` user.name
     }
 
     @Test
     fun getUserByName() {
-        dao.insertUser(User("Sommai"), nectecOrg.id)
+        dao.insertUser(createUser("Sommai"), nectecOrg.id)
 
         dao.getUserByName(nectecOrg.id, "Sommai")!!.name `should be equal to` "Sommai"
     }
