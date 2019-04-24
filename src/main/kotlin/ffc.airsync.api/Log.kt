5989fdde37363931241dd71e81b0e16e1f73f66a
Piruin Panichphol <piruin.p@gmail.com>
2019-04-24 14:06:47 +0700

    Refactor

--- src/main/kotlin/ffc.airsync.api/Log.kt
+++ src/main/kotlin/ffc.airsync.api/Log.kt
@@ -1,12 +1,25 @@
+/*
+ * Copyright (c) 2019 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ffc.airsync.api
 
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-inline fun <reified T> getLoggerC(clazz: T): Logger {
-    return LogManager.getLogger(T::class)
-}
-
-fun Any.getLogger(): Logger {
-    return LogManager.getLogger(this::class.java)
+inline fun <reified T> T.getLogger(): Logger {
+    return LogManager.getLogger(T::class.java)
 }
