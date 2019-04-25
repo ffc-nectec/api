72d11b5af2918e2109ded1f3202d5d892a712363
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-25 14:20:15 +0700

    update otp source.

--- src/main/kotlin/ffc.airsync.api/services/otp/OtpResource.kt
+++ src/main/kotlin/ffc.airsync.api/services/otp/OtpResource.kt
@@ -1,7 +1,24 @@
+/*
+ * Copyright (c) 2019 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package ffc.airsync.api.services.otp
 
 import ffc.airsync.api.services.ORGIDTYPE
-import org.joda.time.DateTime
 import java.util.Date
 import javax.annotation.security.RolesAllowed
 import javax.ws.rs.Consumes
@@ -35,22 +52,12 @@ class OtpResource(
     ): Map<String, Boolean> {
         val timestamp = Date(System.currentTimeMillis())
         val otpString = clientOtp.getValue("otp")
-        val checkCurrentTime = otpDao.isValid(
+        val check = otpDao.isValid(
             orgId = orgId,
             otp = otpString,
             timestamp = timestamp
         )
-        val checkPastTime = otpDao.isValid(
-            orgId = orgId,
-            otp = otpString,
-            timestamp = timestamp.minusMillis(60000)
-        )
-        if (checkCurrentTime || checkPastTime) return mapOf("isValid" to true)
+        if (check) return mapOf("isValid" to true)
         else throw NotAuthorizedException("Cannot auth otp.")
-    }
-
-    private fun Date.minusMillis(millis: Int): Date {
-        val joda = DateTime(time)
-        return Date(joda.minusMillis(millis).millis)
     }
 }
