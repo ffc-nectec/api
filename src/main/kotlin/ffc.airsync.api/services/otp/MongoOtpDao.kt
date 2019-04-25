e3b5bb331cad8be01ed96546121a495a63823f91
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-25 14:20:15 +0700

    update otp source.

--- src/main/kotlin/ffc.airsync.api/services/otp/MongoOtpDao.kt
+++ src/main/kotlin/ffc.airsync.api/services/otp/MongoOtpDao.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2019 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package ffc.airsync.api.services.otp
 
 import com.mongodb.client.model.IndexOptions
@@ -6,6 +24,7 @@ import ffc.airsync.api.services.util.equal
 import ffc.airsync.api.services.util.ignoreException
 import org.bson.Document
 import org.bson.types.ObjectId
+import org.joda.time.DateTime
 import java.util.Date
 import java.util.concurrent.TimeUnit
 
@@ -54,6 +73,13 @@ class MongoOtpDao(
             ?.get("secretKey") as String?
 
     override fun isValid(orgId: String, otp: String, timestamp: Date): Boolean {
-        return otp == (get(orgId, timestamp))
+        val checkCurrentTime = otp == (get(orgId, timestamp))
+        val checkPastTime = otp == (get(orgId, timestamp.minusMillis(60000)))
+        return checkCurrentTime || checkPastTime
+    }
+
+    private fun Date.minusMillis(millis: Int): Date {
+        val joda = DateTime(time)
+        return Date(joda.minusMillis(millis).millis)
     }
 }
--- src/test/kotlin/ffc/airsync/api/services/otp/OtpResourceTest.kt
+++ src/test/kotlin/ffc/airsync/api/services/otp/OtpResourceTest.kt
@@ -1,7 +1,27 @@
+/*
+ * Copyright (c) 2019 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package ffc.airsync.api.services.otp
 
 import com.nhaarman.mockitokotlin2.eq
+import ffc.airsync.api.GsonJerseyProvider
 import ffc.airsync.api.filter.RequireError
+import ffc.airsync.api.filter.SuccessToCreatedResponse
 import org.amshove.kluent.When
 import org.amshove.kluent.`should equal`
 import org.amshove.kluent.any
@@ -28,6 +48,8 @@ class OtpResourceTest : JerseyTest() {
 
         return ResourceConfig()
             .registerClasses(RequireError::class.java)
+            .register(GsonJerseyProvider::class.java)
+            .register(SuccessToCreatedResponse::class.java)
             .register(OtpResource(mouckOtpDao))
     }
 
@@ -49,6 +71,6 @@ class OtpResourceTest : JerseyTest() {
         )
 
         res.readEntity(Map::class.java)["isValid"] `should equal` true
-        res.status `should equal` 200
+        res.status `should equal` 201
     }
 }
