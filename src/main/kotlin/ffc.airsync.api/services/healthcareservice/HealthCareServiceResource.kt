3f6c23c50d900eed318a8da498a5a3ed2083c381
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-09-02 10:43:57 +0700

    fix import

--- src/main/kotlin/ffc.airsync.api/services/healthcareservice/HealthCareServiceResource.kt
+++ src/main/kotlin/ffc.airsync.api/services/healthcareservice/HealthCareServiceResource.kt
@@ -33,7 +33,12 @@ import ffc.entity.User
 import ffc.entity.healthcare.HealthCareService
 import ffc.entity.healthcare.analyze.HealthAnalyzer
 import javax.annotation.security.RolesAllowed
-import javax.ws.rs.*
+import javax.ws.rs.DELETE
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.PUT
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.SecurityContext
